4417effb2be690df947fb4394d14553c2a94f840
Max AR <xabram18@gmail.com>
2019-03-20 00:59:45 -0700

    twitter auth added and jankily tested

--- bot.py
+++ bot.py
@@ -1,9 +1,23 @@
+import json, tweepy
+
 class TwitterBot:
+    secrets = {}
+    api = None
+
     def __init__(self):
         self.importSecrets("./secrets.json")
+        self.initApi()
 
     def importSecrets(self, secretFile):
-        print("import: " + secretFile)
+        f = open(secretFile, "r")
+        self.secrets = json.loads(f.read())
 
     def run(self):
         print("ehllo")
+        user = self.api.me()
+        print (user.name)
+
+    def initApi(self):
+        auth = tweepy.OAuthHandler(self.secrets["apiKey"], self.secrets["apiSecretKey"])
+        auth.set_access_token(self.secrets["accessToken"], self.secrets["accessTokenSecret"])
+        self.api = tweepy.API(auth)
